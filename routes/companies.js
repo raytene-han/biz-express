57ad9d27e59d7188f061177dec9667dbab845cce
Raytene Han <raytenehan@gmail.com>
2022-08-18 14:55:42 -0700

    finished companies routes

--- routes/companies.js
+++ routes/companies.js
@@ -5,7 +5,7 @@ const { NotFoundError } = require("../expressError");
 const db = require("../db");
 const router = express.Router();
 
-/** GET all companies */
+/** GET all companies, returns json {companies: [{code, name}, ...]} */
 router.get('/', async function (req, res) {
   let results = await db.query(
     `SELECT code, name FROM companies;`
@@ -14,7 +14,7 @@ router.get('/', async function (req, res) {
   return res.json({ companies });
 });
 
-/** GET a company */
+/** GET a company, returns json {company: {code, name, description}} */
 router.get('/:code', async function (req, res) {
   let results = await db.query(
     `SELECT code, name, description FROM companies
@@ -25,18 +25,43 @@ router.get('/:code', async function (req, res) {
   return res.json({ company });
 });
 
-/** POST add a company*/
+/** POST add a company, returns json {company: {code, name, description}}*/
 router.post('/', async function (req, res) {
   let { code, name, description } = req.body;
+  // wrap in try/catch and throw badrequesterror
   let results = await db.query(
     `INSERT INTO companies (code, name, description)
     VALUES ($1, $2, $3)
     RETURNING code, name, description`, [code, name, description]
   );
   let company = results.rows[0];
-  return
-})
-/** PUT update a comany*/
-/** DELETE delete a company */
+  return res.status(201).json({ company });
+});
+
+/** PUT update a company, returns json {company: {code, name, description}}*/
+router.put('/:code', async function (req, res) {
+  const code = req.params.code;
+  let { name, description } = req.body;
+  let results = await db.query(
+    `UPDATE companies
+    SET name = $1,
+        description = $2
+    WHERE code = $3
+    RETURNING code, name, description`, [ name, description, code]
+  );
+  let company = results.rows[0];
+  if (!company) throw new NotFoundError();
+  return res.json({ company });
+});
+
+/** DELETE delete a company, returns json {status: "deleted"} */
+router.delete('/:code', async function (req, res) {
+  const response = await db.query(
+    `DELETE FROM companies
+    WHERE code = $1`, [req.params.code]
+  );
+  if (!response.rowCount) throw new NotFoundError();
+  return res.json({ status: "Deleted" });
+});
 
 module.exports = router;
